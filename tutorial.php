eac18d78c70c98713b12a6e859cb829f9f42ddd2
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-10-14 12:15:37 -0700

    My first blog.

---
+++ tutorial.php
@@ -0,0 +1,21 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+	<?php
+
+		$admin = true;
+		$moderator = true;
+		if() {
+
+		}
+
+	?>
+<script type="text/javascript" href="js/bootstrap.js"></script>
+<script type="text/javascript" href="js/bootstrap.min.js"></script>
+</body>
+</html>
