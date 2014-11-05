8fb4598899ac78aa601e6b098346d1661ddc8cdc
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-11-05 12:10:32 -0800

    no message

--- tutorial.php
+++ tutorial.php
@@ -4,3 +4,16 @@
 	require_once(__DIR__ . "/controller/create-db.php");
 	require_once(__DIR__ . "/view/footer.php");
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+
+</body>
+</html>
--- view/form.php
+++ view/form.php
@@ -2,14 +2,17 @@
 
 <form>
 	<div>
+		<!--creates a space for a title-->
 		<label for="title">Title: </label>
 		<input type="text" name="title" />
 	</div>
 	<div>
+		<!--creates a space for a post-->
 		<label for="post">Post: </label>
 		<textarea name="post"></textarea>
 	</div>
 	<div>
+		<!--creates a button that says submit-->
 		<button type="submit">Submit</button>
 	</div>
 </form>
