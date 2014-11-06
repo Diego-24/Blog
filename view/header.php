20a9695763f191de341ecc7833f3d702a96afb44
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-11-06 12:11:38 -0800

    no message

--- view/header.php
+++ view/header.php
@@ -1,6 +1,12 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 </head>
 <body>
+
+</body>
