52844835029a3a8b71d7a85e7b02100821a76f9f
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-11-07 11:08:37 -0800

    no message

--- controller/create-post.php
+++ controller/create-post.php
@@ -2,4 +2,7 @@
 	/*makes sure no weird things happen to the input*/
 	/*INPUT_POST gives us info that is being posted*/
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-	$post;
+	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+
+	echo "<p>Title: $title</p>";
+	echo "<p>Post: $post</p>";
