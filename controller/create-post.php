d9678a8e32cbc473a5e4a869241c52e93d8c9975
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-11-13 12:12:04 -0800

    Completed video 25
    It queries my database to submit

--- controller/create-post.php
+++ controller/create-post.php
@@ -9,8 +9,15 @@
 	$title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 	$post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
 
-	echo "<p>Title: $title</p>";
-	echo "<p>Post: $post</p>";
+	/*runs a query on my dataase.Inserts into my table called posts.Setting the values within post called title and post*/
+	$query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+
+	if($query) {
+		echo "<p>Successfully inserted post: $post</p>";
+	}
+	else {
+		echo "<p>$connection->error</p>";
+	}
 
 	/*closes connection*/
 	$connection->close();
