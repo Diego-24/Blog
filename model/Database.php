55373fb49a61b7f4f2e373aae997cbc0b2e7cf04
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-11-14 12:28:54 -0800

    Completed Video 26
    Created a constructor for our database class

---
+++ model/Database.php
@@ -0,0 +1,9 @@
+<?php
+/*cleaner and more organized code. We use classes so we can create an object*/
+class Database {
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $database;
+}
