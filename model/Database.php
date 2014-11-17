d03d0f2c8b0d303506f062f004500958352158ba
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-11-17 13:25:12 -0800

    Completed videos 27-28
    Created a constructor for my database class

--- model/Database.php
+++ model/Database.php
@@ -6,4 +6,27 @@ class Database {
 	private $username;
 	private $password;
 	private $database;
+
+	/*Defines the constructors. Constructor is called on an object after it has been made.*/
+	public function __construct($host, $username, $password, $database) {
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+	} 
+
+	/*opens the connection*/
+	public function openConnection() {
+
+	}
+
+	/*closes the connection*/
+	public function closeConnection() {
+
+	}
+
+
+	public function query($string) {
+
+	}
 }
