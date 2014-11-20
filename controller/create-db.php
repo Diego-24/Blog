1557e973ac1b88211e2694bf0f7e577ff93e5281
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-11-20 12:20:58 -0800

    completed videos 33-34
    Stored query errors in my database class

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,32 +1,6 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php");
 
-	/*Checking if there was an error connecting to the database.
-	If there is an error the program will die*/
-	if ($connection->connect_error) {
-		die("<p>Error: " . $connection->connect_error . "</p>");
-	}
-	/*Tries to access a database that exists on mySQL server*/
-	$exists = $connection->select_db($database);
-
-	/*checking if we were able to connect to that database.
-	Exclamation point changes it from true to false*/
-	if (!$exists) {
-		/*Sends a command to make a database*/
-		$query = $connection->query("CREATE DATABASE $database");
-
-		/*this if statement echos that the database was created*/
-		if($query) {
-			echo "<p>Successfully creaated database: " . $database . "</p>";
-		}
-
-	}
-	/*this else statement is echoing that my database exists*/
-	else {
-		echo "<p>Database already exists.</p>";
-
-	}
-
 	/*Creates the entire table*/
 	$query = $connection->query("CREATE TABLE posts ("
 		. "id int(11) NOT NULL AUTO_INCREMENT,"
@@ -42,7 +16,3 @@
 	else{
 		echo "<p>$connection->error</p>";
 	}
-
-	$connection->close();
-
-?>
--- model/config.php
+++ model/config.php
@@ -1,4 +1,6 @@
 <?php
+	require_once(__DIR__ . "/database.php");
+
 	/*Sets the path that leads to PHPBasics*/
 	$path = "/PHPBasics/";
 	
