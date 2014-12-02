35ed0e58e698dec1e4a94f8e59fde6ccdfb0d96d
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-12-02 12:11:24 -0800

    completed video 37
    I created a registration form

---
+++ view/register-form.php
@@ -0,0 +1,24 @@
+<form>
+	<div>
+		<!--creates a slot for the user's email-->
+		<label for="email">Email: </label>
+		<input type="text" name="email" />
+	</div>
+
+	<div>
+		<!--creates a slot for the user's username-->
+		<label for="username">Username: </label>
+		<input type="text" name="username" />
+	</div>
+
+	<div>
+		<!--creates a slot for the user's password-->
+		<label for="password">Password: </label>
+		<input type="password" name="password" />
+	</div>
+
+	<div>
+		<!--creates a submit button-->
+		<button type="submit">Submit</button>
+	</div>
+</form>
