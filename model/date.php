e4deb1b4cc95b8c62ec6dcf90b22eeafd7f27090
Diego Sanchez <sanchezdiegoamazing@gmail.com>
2014-12-10 12:14:32 -0800

    Finished up to video 47

--- model/date.php
+++ model/date.php
@@ -4,5 +4,5 @@ $date = new DateTime("today");
 /*creates today"s time*/
 $time = new DateTime('America/Los_Angeles');
 /*echos the date and time*/
-echo $date->format("M-D-Y") . " at " . $time->format("h:i");
+echo $date->format("M/D" . " " . "d/Y") . " at " . $time->format("h:i");
 ?>
--- view/login-form.php
+++ view/login-form.php
@@ -2,16 +2,9 @@
 	require_once(__DIR__ . "/../model/config.php");
 ?>
 
-<h1>Register</h1>
+<h1>Login</h1>
 
-<!--creates a path-->
-<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>">
-	<div>
-		<!--creates a slot for the user's email-->
-		<label for="email">Email: </label>
-		<input type="text" name="email" />
-	</div>
-
+<form method="post" action="<?php echo $path . "controller/login-user.php"?>">
 	<div>
 		<!--creates a slot for the user's username-->
 		<label for="username">Username: </label>
